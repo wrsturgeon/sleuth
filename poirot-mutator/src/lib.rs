025314722039ad14f31b4e450c44fc0651b9a697
Will Sturgeon <79714036+wrsturgeon@users.noreply.github.com>
2023-04-27 16:24:15 -0400

    rename

---
+++ poirot-mutator/src/lib.rs
@@ -0,0 +1,26 @@
+#![deny(warnings)]
+#![warn(clippy::missing_docs_in_private_items)]
+#![allow(unreachable_code)]
+
+mod cover;
+
+use cover::*;
+use proc_macro::TokenStream;
+
+#[proc_macro_attribute]
+pub fn poirot(attr: TokenStream, input: TokenStream) -> TokenStream {
+    match cover_impl(attr.into(), input.into()) {
+        Ok(ts) => ts,
+        Err(e) => e.to_compile_error(),
+    }
+    .into()
+}
+
+// #[proc_macro_attribute]
+// pub fn fn_predicate(attr: TokenStream, input: TokenStream) -> TokenStream {
+//     match fn_predicate_impl(attr.into(), input.into()) {
+//         Ok(ts) => ts,
+//         Err(e) => e.to_compile_error(),
+//     }
+//     .into()
+// }
